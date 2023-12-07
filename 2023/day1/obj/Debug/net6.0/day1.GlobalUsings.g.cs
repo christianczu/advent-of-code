0fe88ff441ba96ff43336d94d77ce07685172046
christianczu <christianalexander.cozianu@student.odisee.be>
2023-12-07 15:26:17 +0100

    first push

---
+++ 2023/day1/obj/Debug/net6.0/day1.GlobalUsings.g.cs
@@ -0,0 +1,18 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace day1 // Note: actual namespace depends on the project name.
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

56d74134a7b4c9b0ce7f2ff36ded05918a183ca3
christianczu <christianalexander.cozianu@student.odisee.be>
2023-12-08 11:01:52 +0100

    i give up

--- 2023/day1/Program.cs
+++ 2023/day1/Program.cs
@@ -43,20 +43,24 @@ namespace day1
 
             
             var numbers = new List<int>();
-            var lines = File.ReadLines("D:\\Desktop\\advent-of-code\\2023\\day1\\input_part2_example.txt");
+            var lines = File.ReadLines("C:\\Users\\chris\\Desktop\\advent-of-code\\2023\\day1\\input_part2_example.txt");
             foreach (var line in lines)
             {
-                foreach (var pair in numberDict)
-                {
-                    string pattern = @"\b" + pair.Key + @"\b";
-                    Regex regex = new Regex(pattern);
+                string modifiedLine = line;
 
-                    if (regex.IsMatch(line))
+                foreach (var entry in numberDict.OrderByDescending(kv => kv.Key.Length))
+                {
+                    int index = modifiedLine.IndexOf(entry.Key, StringComparison.OrdinalIgnoreCase);
+                    if (index != -1)
                     {
-                        Console.WriteLine(regex.Replace(line, pair.Value, 1)); 
+                        modifiedLine = modifiedLine.Substring(0, index) + entry.Value + modifiedLine.Substring(index + entry.Key.Length);
+                        break; // Break after replacing the first occurrence
                     }
                 }
-                
+
+                string result = Regex.Replace(modifiedLine, @"[^\d+]", "");
+                Console.WriteLine(result);
+
 
                 //string number, number1, number2;
                 //string result = Regex.Replace(line, @"[^\d+]", "");

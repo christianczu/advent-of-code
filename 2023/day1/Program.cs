610f0e8b83604002f0da5d2d5629129fcb6854aa
christianczu <christianalexander.cozianu@student.odisee.be>
2023-12-10 19:26:04 +0100

    last commit... i lied

--- 2023/day1/Program.cs
+++ 2023/day1/Program.cs
@@ -30,44 +30,36 @@ namespace day1
 
             var numberDict = new Dictionary<string, string>()
             {
-                { "nine", "9" },
-                { "eight", "8" },
-                { "seven", "7" },
-                { "six", "6" },
-                { "five", "5" },
-                { "four", "4" },
-                { "three", "3" },
-                { "two", "2" },
-                { "one", "1" },
+                { "nine", "n9e" },
+                { "eight", "e8t" },
+                { "seven", "s7n" },
+                { "six", "s6x" },
+                { "five", "f5e" },
+                { "four", "f4r" },
+                { "three", "t3e" },
+                { "two", "t2o" },
+                { "one", "o1e" },
             };
 
             
             var numbers = new List<int>();
-            var lines = File.ReadLines("C:\\Users\\chris\\Desktop\\advent-of-code\\2023\\day1\\input_part2_example.txt");
+            var lines = File.ReadLines("D:\\Desktop\\advent-of-code\\2023\\day1\\input_part1.txt");
             foreach (var line in lines)
             {
-                string modifiedLine = line;
 
-                foreach (var entry in numberDict.OrderByDescending(kv => kv.Key.Length))
-                {
-                    int index = modifiedLine.IndexOf(entry.Key, StringComparison.OrdinalIgnoreCase);
-                    if (index != -1)
-                    {
-                        modifiedLine = modifiedLine.Substring(0, index) + entry.Value + modifiedLine.Substring(index + entry.Key.Length);
-                        break; // Break after replacing the first occurrence
-                    }
-                }
+                var output = new StringBuilder(line);
 
-                string result = Regex.Replace(modifiedLine, @"[^\d+]", "");
-                Console.WriteLine(result);
+                foreach (var kvp in numberDict)
+                    output.Replace(kvp.Key, kvp.Value);
 
+                var numbered = output.ToString();
 
-                //string number, number1, number2;
-                //string result = Regex.Replace(line, @"[^\d+]", "");
-                //number1 = result.Substring(0, 1);
-                //number2 = Convert.ToString(result[result.Length - 1]);
-                //number = number1 + number2;
-                //numbers.Add(Convert.ToInt32(number));
+                string number, number1, number2;
+                var result = Regex.Replace(numbered, @"[^\d+]", "");
+                number1 = result.Substring(0, 1);
+                number2 = Convert.ToString(result[result.Length - 1]);
+                number = number1 + number2;
+                numbers.Add(Convert.ToInt32(number));
             }
 
 
---
+++ 2023/day2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
+using System.Text;
+using System.Net;
+using System.Diagnostics.CodeAnalysis;
+
+namespace day2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] color = { 'r', 'g', 'b' };
+            var numbers = new List<int>();
+            var lines = File.ReadLines("D:\\Desktop\\advent-of-code\\2023\\day2\\input1_example.txt");
+            foreach (var line in lines)
+            {
+                int gameId = Convert.ToInt32(line.Substring(line.IndexOf(' ') + 1, line.IndexOf(':') - line.IndexOf(' ') - 1));
+                var r = new List<int>();
+                var g = new List<int>();
+                var b = new List<int>();
+                foreach (char c in color)
+                {
+                    string pattern = $@"\b{Regex.Escape(c.ToString())}\w+";
+                    foreach (Match match in Regex.Matches(line, pattern))
+                    {
+                         switch (c)
+                        {
+                            case 'r':
+                                Console.Write(line.Substring(match.Index - 3, 3));
+                                r.Add(Convert.ToInt32(line.Substring(match.Index - 3, 3)));
+                                break;
+                            case 'g':
+                                g.Add(Convert.ToInt32(line.Substring(match.Index - 3, 3)));
+                                break;
+                            case 'b':
+                                b.Add(Convert.ToInt32(line.Substring(match.Index - 3, 3)));
+                                break;
+                        }
+                    }
+                }
+                if (r.Sum() <= 12 && g.Sum() <= 13 && b.Sum() <= 14)
+                {
+                    numbers.Add(gameId);
+                }
+                
+            }
+
+            //Console.WriteLine(numbers.Sum());
+            Console.Read();
+        }
+    }
+}
